2df30503b074ae0f222b4901096f221b7407a8ce
ladcva <ladcva@yahoo.com>
2021-12-01 17:11:42 +0700

    Init

--- index.php
+++ index.php
@@ -10,16 +10,16 @@ use Hitrov\OciApi;
 use Hitrov\OciConfig;
 
 $config1 = new OciConfig(
-    getenv('OCI_REGION') ?: 'us-phoenix-1', // region
-    getenv('OCI_USER_ID') ?: 'ocid1.user.oc1..aaaaaaaa***', // user
-    getenv('OCI_TENANCY_ID') ?: 'ocid1.tenancy.oc1..aaaaaaaaa***', // tenancy
-    getenv('OCI_KEY_FINGERPRINT') ?: '42:b1:***:5b:2c', // fingerprint
-    getenv('OCI_PRIVATE_KEY_FILENAME') ?: "oracleidentitycloudservice_oracle-***.pem", // key_file
-    getenv('OCI_AVAILABILITY_DOMAIN') ?: 'jYtI:PHX-AD-1', // availabilityDomain
-    getenv('OCI_SUBNET_ID') ?: 'ocid1.subnet.oc1.phx.aaaaaaaa***', // subnetId
-    getenv('OCI_IMAGE_ID') ?: 'ocid1.image.oc1.phx.aaaaaaaay***', // imageId
-    (int) getenv('OCI_OCPUS') ?: 4,
-    (int) getenv('OCI_MEMORY_IN_GBS') ?: 24
+    getenv('OCI_REGION') ?: 'ap-singapore-1', // region
+    getenv('OCI_USER_ID') ?: 'ocid1.user.oc1..aaaaaaaagvjovj2n65q4ulyhh4gthokuuvu5pnltcuamjilims2fg2wdujoq', // user
+    getenv('OCI_TENANCY_ID') ?: 'ocid1.tenancy.oc1..aaaaaaaa3a4gpk4a5qfaeidw7qbr5rpjht2w57tz7qnh24wemrgdntoco6jq', // tenancy
+    getenv('OCI_KEY_FINGERPRINT') ?: '8e:08:e1:83:ce:75:ee:51:4a:8b:09:44:5b:08:2e:88', // fingerprint
+    getenv('OCI_PRIVATE_KEY_FILENAME') ?: "ladcva-12-01-07-27.pem", // key_file
+    getenv('OCI_AVAILABILITY_DOMAIN') ?: 'Cplu:AP-SINGAPORE-1-AD-1', // availabilityDomain
+    getenv('OCI_SUBNET_ID') ?: 'ocid1.subnet.oc1.ap-singapore-1.aaaaaaaaosyfpkxwbxaqwzem2d2qnj2twmdsjrxc2bhiz23km2eol4jcnvla', // subnetId
+    getenv('OCI_IMAGE_ID') ?: 'ocid1.image.oc1.ap-singapore-1.aaaaaaaa3vsfhyvqje5yyn2vrr52tfnm5j2xovxyl2uojexvead5mesrh7ba', // imageId
+    (int) getenv('OCI_OCPUS') ?: 16,
+    (int) getenv('OCI_MEMORY_IN_GBS') ?: 96
 );
 
 $configs = [
@@ -56,7 +56,7 @@ foreach ($configs as $config) {
         continue;
     }
 
-    $sshKey = getenv('OCI_SSH_PUBLIC_KEY') ?: 'ssh-rsa AAAAB3NzaC1...p5m8= ubuntu@localhost'; // ~/.ssh/id_rsa.pub contents
+    $sshKey = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCgK3gH5L1msTr8F1S4vffPV178d7ZU9UOWjhVjdBAODATA1OA/Emo0mgLQPR0VpzA2FRgGJEyUWrkY/+dh5wzYsQ2oYoVrPXN6dxuKTCuv4iznTcRqUU9Xu0RA1gUUqX3hXeCsaQ58ZVSlvxW1Bc+0WFmpdnaBcZlrN58aK41G1sMvYiY5W1aX+Xa07kBuvZlJyv53iyOV89K3DlFp5b6xbar5IMnEPbMVgdRGriRx5roaJ+hUjFGp1cmwrd81Q2GrTz8xalJ8qk8rPnfDvkaysUGeSsu5Gps32QIBCD1YEK4PLMXYTwbHcHWfonkeIemftuAf8EnJOjVcwy5l4cYCARVYYFX1DI1Thir3n3cEa9T2W0XnlDgoD1oDfXQjceC2CBJ7N3cJFXj9diLQUqJW80kOwu4E9Uvdie6gnrOFDJJQDHsjiBx+I9dBM7HiPt5DvvPGNxXOzXi1VMfD8gn1mejMqwhpht0K7sX9b8cU1QGHG04HmWswd0ROxivFXE8= mac@macs-MacBook-Pro.local'; // ~/.ssh/id_rsa.pub contents
     [ $response, $error, $info ] = $api->createInstance($config, $shape, $sshKey);
 
     $r = json_decode($response, true);
--- src/OciConfig.php
+++ src/OciConfig.php
@@ -5,14 +5,14 @@ namespace Hitrov;
 
 class OciConfig
 {
-    public string $region = '';
-    public string $ociUserId = '';
-    public string $tenancyId = '';
-    public string $keyFingerPrint = '';
+    public string $region = 'ap-singapore-1';
+    public string $ociUserId = 'ocid1.user.oc1..aaaaaaaagvjovj2n65q4ulyhh4gthokuuvu5pnltcuamjilims2fg2wdujoq';
+    public string $tenancyId = 'ocid1.tenancy.oc1..aaaaaaaa3a4gpk4a5qfaeidw7qbr5rpjht2w57tz7qnh24wemrgdntoco6jq';
+    public string $keyFingerPrint = '8e:08:e1:83:ce:75:ee:51:4a:8b:09:44:5b:08:2e:88';
     public string $privateKeyFilename = '';
-    public string $availabilityDomain = '';
-    public string $subnetId = '';
-    public string $imageId = '';
+    public string $availabilityDomain = 'Cplu:AP-SINGAPORE-1-AD-1';
+    public string $subnetId = 'ocid1.subnet.oc1.ap-singapore-1.aaaaaaaaosyfpkxwbxaqwzem2d2qnj2twmdsjrxc2bhiz23km2eol4jcnvla';
+    public string $imageId = 'ocid1.image.oc1.ap-singapore-1.aaaaaaaa3vsfhyvqje5yyn2vrr52tfnm5j2xovxyl2uojexvead5mesrh7ba';
     public ?int $ocpus;
     public ?int $memoryInGBs;
 
@@ -25,8 +25,8 @@ class OciConfig
         string $availabilityDomain,
         string $subnetId,
         string $imageId,
-        int $ocups = 4,
-        int $memoryInGBs = 24
+        int $ocups = 16,
+        int $memoryInGBs = 94
     )
     {
         $this->region = $region;
